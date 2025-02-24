3d5217153ef68b34b1986697736e7e98a8a3f843
atete <n.atete@alustudent.com>
2025-02-24 20:11:27 +0200

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // ✅ Import Router components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import "./style/dark.scss"
 import { DarkModeContext } from "./context/darkModeContext";
